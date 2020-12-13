d081661b59ec489c0b5d1e8ec19a12108a7bf560
Joao Maia <15joaomaia@gmail.com>
2020-12-13 18:42:39 -0300

    Learning-bootstrap-4

---
+++ subscribe/assets/js/script.js
@@ -0,0 +1,9 @@
+$(()=>{  
+    window.sr =   ScrollReveal();
+    sr.reveal('.anime', 
+        { 
+            distance: '200px',
+            origin:'right'                 
+        }            
+    ); 
+});
