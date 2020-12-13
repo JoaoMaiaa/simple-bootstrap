191e37203f02c2282c7cc1c4ecd8e4b35f5ba800
Joao Maia <15joaomaia@gmail.com>
2020-12-13 18:09:32 -0300

    Learning-bootstrap-4

---
+++ blog/assets/js/script.js
@@ -0,0 +1,59 @@
+$(()=>{
+    $('.slider').slick({
+        centerMode: true,
+        autoplay: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        slidesToSlow:1,
+        arrows: false,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
+
+      $('.slider-2').slick({
+        // centerMode: true,
+        autoplay: true,
+        // centerPadding: '60px',
+        slidesToShow: 3,
+        slidesToSlow:3,
+        arrows: false,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
+});
