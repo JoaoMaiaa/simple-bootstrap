652cd91044ee630f58f7c1fead481984178987f3
Joao Maia <15joaomaia@gmail.com>
2020-12-13 18:18:59 -0300

    Learning-bootstrap-4

---
+++ landing-page/assets/js/script.js
@@ -0,0 +1,17 @@
+//$(()=>{
+//     $('.navbar .nav-link').on('click',function(e){
+//         e.preventDefault();
+
+//         /*elemento html(tag)*/
+//         // let position = e.target;
+
+//         let href = $(this).attr('href');
+
+//         let target = $(href).offset().top;
+
+//         $('html , body').animate({
+//             scrollTop : target
+//         },500);
+
+//     });
+//});
